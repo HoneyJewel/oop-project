a4c680e2cab58d203489d8a46b4448cdfb5f8b3b
HoneyJewel <hone.gaan.coc@phinmaed.com>
2024-10-07 14:32:58 +0800

    Add files via upload

---
+++ jewel/sidebar.php
@@ -0,0 +1,19 @@
+<!-- Sidebar -->
+<div class="sidebar" id="mySidebar">
+<div class="side-header">
+    <img src="./assets/images/logo.png" width="120" height="120" alt="Swiss Collection"> 
+    <h5 style="margin-top:10px;">Hello User!</h5>
+</div>
+
+<hr style="border:1px solid; background-color:#8a7b6d; border-color:#3B3131;">
+    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
+    <a href="#upcoming"  onclick="showUpcoming()" ><i class="fa fa-users"></i> Upcoming Events</a>
+    <a href="#current"   onclick="showCurrent()" ><i class="fa fa-th-large"></i> Current Events</a>
+  <!---->
+</div>
+ 
+<div id="main">
+    <button class="openbtn" onclick="openNav()"><i class="fa fa-home"></i></button>
+</div>
+
+
